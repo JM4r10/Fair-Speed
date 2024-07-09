5029c2e3e79a250602e074bbe21ba84bd46d97d1
JM4r10 <bakyoram@gmail.com>
2024-07-09 13:31:57 -0500

    Update GameManager.cs
    Añadí lógica para cambiar escenas:
    StartGame() cambia a Main Scene
    Defeat() cambia a Game Over Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+    [SerializeField] private float gameSpeed;
+    private Scene currentScene;
+    private int score;
+
+    public float GameSpeed
     {
-        
+        get => gameSpeed;
+        set => gameSpeed = value;
+    }
+    public int Score { get => score; }
+
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    public void Defeat()
+    {
+        if (!(currentScene.name == "Main Scene")) return;
+
+        GameSpeed = 0;
+        SceneManager.LoadScene("Game Over Scene");
+    }
+
+    public void StartGame()
+    {
+        if (!(currentScene.name == "Start Scene")) return;
+
+        SceneManager.LoadScene("Main Scene");
     }
 }
